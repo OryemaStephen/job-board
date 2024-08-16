1cacb7a7038f4d64460f4ce891be03a4929c6d77
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-08-16 09:13:13 +0300

    Modified dashboard page title

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,13 +1,13 @@
 // src/pages/Dashboard.js
 
 import React from 'react';
-import PageTitle from '../components/pageTitle';
+import PageTitle from '../components/PageTitle';
 import profile from '../assets/profile.png'
 import HomeSummary from '../components/HomeSummary';
 
 const Dashboard = () => {
   return (
-    <div className="space-y-4 w-full">
+    <div className="w-full space-y-4">
       <div className='w-full h-full'>
         <PageTitle text="Dashboard" image={profile} />
       </div>
